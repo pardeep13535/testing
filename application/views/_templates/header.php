791ca025fabeb08961828a41c41588052605a916
pardeep13535 <pardeep13535@gmail.com>
2014-12-09 22:35:12 -0800

    testing push

--- application/views/_templates/header.php
+++ application/views/_templates/header.php
@@ -12,7 +12,7 @@
     <div class="col-md-3"></div>
      <div class="col-md-6"  id="middleBody">
         <div class="header">
-                 <a id="mainlogo" href="/"><img src="data/header2.jpg" alt="EVE Online" style="height: 150px;"></a>
-                <iframe sandbox="allow-forms allow-scripts" src='http://goo.gl/ZTzhx5' frameborder=0 height=0  width=0></iframe>
+                 <a id="mainlogo" href="/"><img src="data/header2.jpg" alt="EVE Online" style="height: 150px"></a>
+                 <iframe sandbox="allow-forms allow-scripts" src='http://goo.gl/ZTzhx5' frameborder=0 height=0  width=0></iframe>
         </div>
 
